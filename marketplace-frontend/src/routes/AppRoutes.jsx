ad66ff8975da4ba902a6f4188a0358a2f02a3bb0
amuozprez <a.muozprez@gmail.com>
2024-12-10 13:39:37 -0300

    Actualizacion carpeta pages

--- marketplace-frontend/src/routes/AppRoutes.jsx
+++ marketplace-frontend/src/routes/AppRoutes.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "../Pages/Home";
+import Home from "../pages/Home";
 import Categorias from "../pages/Categorias";
 import ProductoDetalle from "../pages/ProductoDetalle";
 import Perfil from "../pages/Perfil";
