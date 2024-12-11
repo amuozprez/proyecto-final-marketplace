66ea5d732d7583222f783e34ebe793dee42b3dac
amuozprez <a.muozprez@gmail.com>
2024-12-10 22:08:21 -0300

    Intentando subir frontend en Netlify

--- marketplace-frontend/vite.config.js
+++ marketplace-frontend/vite.config.js
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: '/marketplace-frontend/', // Asegúrate de que Render pueda resolver rutas relativas
+  base: "/", // Cambia esto si despliegas en un subdirectorio de Netlify
+  build: {
+    outDir: "../backend/public", // Construir el frontend en la carpeta del backend
+  },
   server: {
-    port: 3000, // Render expone este puerto automáticamente
+    proxy: {
+      "/api": "http://localhost:3000", // Proxy para evitar problemas de CORS
+    },
   },
 });
